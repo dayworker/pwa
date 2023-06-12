741029d0cb1d61781027b22b0c701fbc70145d76
YoungJin Kim <dayworker@wrapcore.co.kr>
2023-06-12 15:12:04 +0900

    'edit'

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', function(e) {
-  // 여기에 코드를 넣지 않아도 설치는 가능하다.
-})
+self.addEventListener("fetch", function (e) {
+	// 여기에 코드를 넣지 않아도 설치는 가능하다.
+});
+
+self.addEventListener("push", () => {
+	self.registration.showNotification("Hello world!", {});
+});
